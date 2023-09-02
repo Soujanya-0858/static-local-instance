3e20a77bc1061eaed94605c5a658ef47fea7eea6
Soujanya <anipeddisoujanya@gmail.com>
2023-09-02 18:24:58 +0530

    swaps without third variable

---
+++ swapping/src/swapping/swapp.java
@@ -0,0 +1,16 @@
+//swapping two numbers without using third variables
+package swapping;
+
+public class swapp {
+
+	public static void main(String[] args) {
+		int a=40,b=30;
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("a:" +a);
+		System.out.println("b:" +b);
+
+	}
+
+}
