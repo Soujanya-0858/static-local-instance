bc2b10ace58fcc1f27a2bf03a1e9ac46b5f1b019
Soujanya <anipeddisoujanya@gmail.com>
2023-09-02 18:21:28 +0530

    swapping

---
+++ Static/src/st/variables/swap.java
@@ -0,0 +1,18 @@
+//swapping two numbers with using third variable  
+package st.variables;
+
+public class swap {
+
+	public static void main(String[] args) {
+		int a=40,b=20,c;
+		c=a;
+		a=b;
+		b=c;
+		
+		System.out.println("a:" +a);
+		System.out.println("b:"  +b);
+		
+
+	}
+
+}
